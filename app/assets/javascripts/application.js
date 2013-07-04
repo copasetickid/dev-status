4532d0b3fd8f401065a33cd0580ec00b0aa2aa5f
Rushaine McBean <codekid24@gmail.com>
2013-07-04 18:22:50 -0400

    Initial begins of html, css, & js structure to the application layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,6 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
+//= require lib/jquery-2.0.2
+//= require lib/underscore-1.4.4
 //= require_tree .
